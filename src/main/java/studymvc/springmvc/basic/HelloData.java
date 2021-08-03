4c5d78bd22e6cf1326285c097eb422d76f2f0564
dayle998 <dayle998@naver.com>
2021-08-03 16:04:36 +0900

    @ModelAttribute을 통해 요청 파라미터 객체로 받는법

---
+++ src/main/java/studymvc/springmvc/basic/HelloData.java
@@ -0,0 +1,10 @@
+package studymvc.springmvc.basic;
+
+import lombok.Data;
+
+@Data
+public class HelloData {
+
+    private String username;
+    int age;
+}
---
+++ src/main/java/studymvc/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,24 @@
+package studymvc.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import studymvc.springmvc.basic.HelloData;
+
+@RestController
+@Slf4j
+public class RequestParamController {
+
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+}
